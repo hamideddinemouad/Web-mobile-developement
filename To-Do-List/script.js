ea98b0dad5a0e2cf76bc91541eff3c630353b0e8
hamideddine <hamideddinemouad@gmail.com>
2024-10-29 17:25:05 +0100

    added-some-options

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,10 +1,26 @@
 const myButton = document.getElementById('myButton');
 const options = document.getElementById('options');
+const newNoteButton = document.getElementById('newNoteButton');
+const newNoteForm = document.getElementById('newNote');
+const cancel = document.getElementById('cancel');
 console.log(myButton);
 console.log(options);
+console.log(newNoteButton);
+console.log(newNoteForm);
 
 myButton.addEventListener("click",() => 
 {
     // alert('buttonclicked');
     options.classList.toggle("hidden");
 });
+
+newNoteButton.addEventListener("click", () =>
+{
+    newNoteForm.classList.toggle("hidden");
+})
+
+cancel.addEventListener("click", () =>
+{
+        newNoteForm.classList.toggle("hidden");
+})
+    

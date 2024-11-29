847ab12b4cc5c5a07fac2c65fae67ef95324243c
Hamideddine Mouad <hamideddine.mouad@gmail.com>
2024-11-29 15:02:34 +0100

    still strying to change players

--- Fut/dist/script.dev.js
+++ Fut/dist/script.dev.js
@@ -5,8 +5,7 @@
 }
 
 function insertDots() {
-  var divs = document.querySelectorAll("section div"); // console.log(divs);
-  // let dot = document.createElement('div');
+  var divs = document.querySelectorAll("section div"); // let dot = document.createElement('div');
 
   var dot = "<div class=\"dot\">CDM</div>";
   divs.forEach(function (div) {
@@ -52,11 +51,10 @@ function insertFormation(formation) {
 }
 
 function fourThreeThreeDefault(jsonArrayFttd) {
-  var mainHtml = document.querySelector("main"); // console.log(mainHtml);
-
+  var mainHtml = document.querySelector("main");
   var formationSection = document.createElement("section");
   formationSection.classList.add("formation");
-  formationSection.innerHTML = " \n            <div class=\"GK\">".concat(fillpositionrand("GK", jsonArrayFttd), "<div class=\"dot\">GK</div></div>\n\n            <div class=\"CBL\">").concat(fillpositionrand("CB", jsonArrayFttd), "<div class=\"dot\">CBL</div></div>\n\n            <div class=\"CBR\">").concat(fillpositionrand("CB", jsonArrayFttd), "<div class=\"dot\">CBR</div></div>\n\n            <div class=\"LB\">").concat(fillpositionrand("LB", jsonArrayFttd), " <div class=\"dot\">LB</div></div>\n\n            <div class=\"RB\"> ").concat(fillpositionrand("RB", jsonArrayFttd), "<div class=\"dot\">RB</div></div>\n\n            <div class=\"MDF\">").concat(fillpositionrand("CM", jsonArrayFttd), "<div class=\"dot\">MDF</div></div>\n\n            <div class=\"ML\">").concat(fillpositionrand("CM", jsonArrayFttd), "<div class=\"dot\">ML</div></div>\n\n            <div class=\"MR\">").concat(fillpositionrand("CM", jsonArrayFttd), "<div class=\"dot\">MR</div></div>\n\n            <div class=\"STM\">").concat(fillpositionrand("ST", jsonArrayFttd), "<div class=\"dot\">STM</div></div>\n\n            <div class=\"FWR\">").concat(fillpositionrand("RW", jsonArrayFttd), "<div class=\"dot\">FWR</div></div>\n\n            <div class=\"FWL\">").concat(fillpositionrand("LW", jsonArrayFttd), "<div class=\"dot\">FWL</div></div> \n            ");
+  formationSection.innerHTML = "\n            <div class=\"GK\">".concat(fillpositionrand("GK", jsonArrayFttd), "<div class=\"dot\">GK</div></div>\n\n            <div class=\"CBL\">").concat(fillpositionrand("CB", jsonArrayFttd), "<div class=\"dot\">CB</div></div>\n\n            <div class=\"CBR\">").concat(fillpositionrand("CB", jsonArrayFttd), "<div class=\"dot\">CB</div></div>\n\n            <div class=\"LB\">").concat(fillpositionrand("LB", jsonArrayFttd), " <div class=\"dot\">LB</div></div>\n\n            <div class=\"RB\"> ").concat(fillpositionrand("RB", jsonArrayFttd), "<div class=\"dot\">RB</div></div>\n\n            <div class=\"MDF\">").concat(fillpositionrand("CM", jsonArrayFttd), "<div class=\"dot\">CM</div></div>\n\n            <div class=\"ML\">").concat(fillpositionrand("CM", jsonArrayFttd), "<div class=\"dot\">CM</div></div>\n\n            <div class=\"MR\">").concat(fillpositionrand("CM", jsonArrayFttd), "<div class=\"dot\">CM</div></div>\n\n            <div class=\"STM\">").concat(fillpositionrand("ST", jsonArrayFttd), "<div class=\"dot\">ST</div></div>\n\n            <div class=\"FWR\">").concat(fillpositionrand("RW", jsonArrayFttd), "<div class=\"dot\">RW</div></div>\n\n            <div class=\"FWL\">").concat(fillpositionrand("LW", jsonArrayFttd), "<div class=\"dot\">LW</div></div>\n            \n\n            ");
   mainHtml.appendChild(formationSection);
 }
 
@@ -72,7 +70,6 @@ function fillpositionrand(posStr, jsonArrayFpr) {
 
       if (player.position === posStr) {
         player_found = player;
-        console.log(player_found);
         break;
       }
     }
@@ -91,9 +88,12 @@ function fillpositionrand(posStr, jsonArrayFpr) {
     }
   }
 
-  console.log("from fillpositionrand() player_found =" + player_found);
   var playersBadge = "\n                <div class=\"playerCard\">\n                <div class=\"rating\"> <span class=\"ratingnum\">".concat(player_found.rating, "</span> <span class=\"position\">").concat(player_found.position, "</span></div>\n                <div class=\"ppicture\"><img src=\"").concat(player_found.photo, "\" alt=\"\"></div>\n                <div class=\"pname\">").concat(player_found.name, "</div>\n                <div class=\"PAC\">PAC ").concat(player_found.pace, "</span></div>\n                <div class=\"SHO\">SHO ").concat(player_found.shooting, "</div>\n                <div class=\"PAS\">PAS ").concat(player_found.passing, "</div>\n                <div class=\"DRI\">DRI ").concat(player_found.dribbling, "</div>\n                <div class=\"DEF\">DEF ").concat(player_found.defending, "</div>\n                <div class=\"PHY\">PHY ").concat(player_found.physical, "</div>\n                <div class=\"logos\">\n                <div class=\"flag\"><img  src=\"").concat(player_found.flag, "\" alt=\"\"></div>\n                <div class=\"logo\"><img class=\"logo\" src=\"").concat(player_found.logo, "\" alt=\"\"></div>\n                <div class=\"club\"><img  src=\"").concat(player_found.logo, "\" alt=\"\"></div>\n                </div>\n            </div>\n            ");
   return playersBadge;
+}
+
+function checkdot() {
+  console.log(positionDot);
 }
 
 function Apicons() {
@@ -122,8 +122,112 @@ function Apicons() {
   });
 }
 
+function renderPlayerModal(arrplayersRpm, pos) {
+  var modal = document.querySelector(".modal");
+  var _iteratorNormalCompletion2 = true;
+  var _didIteratorError2 = false;
+  var _iteratorError2 = undefined;
+
+  try {
+    for (var _iterator2 = arrplayersRpm[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+      var player = _step2.value;
+      var playersBadge = document.createElement("div");
+      playersBadge.classList.add(pos);
+      playersBadge.innerHTML = "\n            <div class=\"playerCard\">\n            <div class=\"rating\"> <span class=\"ratingnum\">".concat(player.rating, "</span> <span class=\"position\">").concat(player.position, "</span></div>\n            <div class=\"ppicture\"><img src=\"").concat(player.photo, "\" alt=\"\"></div>\n            <div class=\"pname\">").concat(player.name, "</div>\n            <div class=\"PAC\">PAC ").concat(player.pace, "</span></div>\n            <div class=\"SHO\">SHO ").concat(player.shooting, "</div>\n            <div class=\"PAS\">PAS ").concat(player.passing, "</div>\n            <div class=\"DRI\">DRI ").concat(player.dribbling, "</div>\n            <div class=\"DEF\">DEF ").concat(player.defending, "</div>\n            <div class=\"PHY\">PHY ").concat(player.physical, "</div>\n            <div class=\"logos\">\n            <div class=\"flag\"><img  src=\"").concat(player.flag, "\" alt=\"\"></div>\n            <div class=\"logo\"><img class=\"logo\" src=\"").concat(player.logo, "\" alt=\"\"></div>\n            <div class=\"club\"><img  src=\"").concat(player.logo, "\" alt=\"\"></div>\n            </div>\n            </div>\n            <div class=\"dot\">").concat(player.position, "</div>\n        "); // playerinmodal = querySelector(".modal ")
+
+      modal.appendChild(playersBadge);
+    }
+  } catch (err) {
+    _didIteratorError2 = true;
+    _iteratorError2 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
+        _iterator2["return"]();
+      }
+    } finally {
+      if (_didIteratorError2) {
+        throw _iteratorError2;
+      }
+    }
+  }
+} // let dotinmodal = document.querySelectorAll(".modal .dot");
+// Array.from(dotinmodal).forEach(dot => 
+// {
+//     dot.addEventListener("click", () =>
+//     {
+//         // element.parentNode.removeChild(element);
+//         let formationSection = document.querySelector(".formation");
+//         let captured = document.querySelector(`.formation .${pos}`)
+//         // console.log("captured below")
+//         captured.parentNode.removeChild(captured);
+//         // console.log(captured);
+//         captured.remove();
+//         let modal = document.querySelector(".modal")
+//         let cardInModal = document.querySelector(`.modal .${pos}`);
+//         console.log("cardinModal" +cardInModal);
+//         // console.log("cardinModal" + cardInModal);
+//         formationSection.appendChild(cardInModal);
+//         formationSection.style.display = "grid";
+//         modal.style.display = "none";
+//     let dot = document.querySelectorAll(".dot");
+//     Array.from(dot).forEach(doty => 
+//         {
+//             doty.addEventListener("click", ()=>
+//             {
+//                 let formationSection = document.querySelector(".formation")
+//                 let modal = document.querySelector(".modal");
+//                 formationSection.style.display = "none";
+//                 modal.style.display = "flex";
+//                 positionDot = doty.innerText;
+//                 // console.log("doty parent" + doty.parentElement.className);
+//                 renderPlayerModal(showAllPos(positionDot, arrplayersRpm), doty.parentElement.className);
+//                 // showAllPos(positionDot, jsonArr);
+//                 // console.log(positionDot);
+//                 // console.log(formationSection);
+//             })
+//         }
+//         )
+// }
+// )
+//     })
+// })}
+
+
+function showAllPos(posStr, jsonArrSap) {
+  var arrplayers = [];
+  var _iteratorNormalCompletion3 = true;
+  var _didIteratorError3 = false;
+  var _iteratorError3 = undefined;
+
+  try {
+    for (var _iterator3 = jsonArrSap[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+      var player = _step3.value;
+
+      if (player.position === posStr) {
+        arrplayers.push(player);
+      }
+    }
+  } catch (err) {
+    _didIteratorError3 = true;
+    _iteratorError3 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion3 && _iterator3["return"] != null) {
+        _iterator3["return"]();
+      }
+    } finally {
+      if (_didIteratorError3) {
+        throw _iteratorError3;
+      }
+    }
+  }
+
+  return arrplayers;
+}
+
 function main() {
-  var jsonArr;
+  var jsonArr, positionDot, dot;
   return regeneratorRuntime.async(function main$(_context2) {
     while (1) {
       switch (_context2.prev = _context2.next) {
@@ -133,10 +237,23 @@ function main() {
 
         case 2:
           jsonArr = _context2.sent;
-          console.log(jsonArr);
-          fourThreeThreeDefault(jsonArr); // fillpositionrand("CB", jsonArray);
+          fourThreeThreeDefault(jsonArr);
+          dot = document.querySelectorAll(".dot");
+          Array.from(dot).forEach(function (doty) {
+            doty.addEventListener("click", function () {
+              var formationSection = document.querySelector(".formation");
+              var modal = document.querySelector(".modal");
+              formationSection.style.display = "none";
+              modal.style.display = "flex";
+              positionDot = doty.innerText; // console.log("doty parent" + doty.parentElement.className);
 
-        case 5:
+              renderPlayerModal(showAllPos(positionDot, jsonArr), doty.parentElement.className); // showAllPos(positionDot, jsonArr);
+              // console.log(positionDot);
+              // console.log(formationSection);
+            });
+          }); // fillpositionrand("CB", jsonArray);
+
+        case 6:
         case "end":
           return _context2.stop();
       }
--- Fut/script.js
+++ Fut/script.js
@@ -3,7 +3,7 @@
 function insertDots()
 {
     let divs = document.querySelectorAll("section div");
-    // console.log(divs);
+
     // let dot = document.createElement('div');
     let dot = `<div class="dot">CDM</div>`
     divs.forEach(div => (div.innerHTML = dot + 1));
@@ -48,31 +48,32 @@ function insertFormation(formation)
 function fourThreeThreeDefault(jsonArrayFttd)
 {
     let mainHtml = document.querySelector("main");
-    // console.log(mainHtml);
     let formationSection = document.createElement("section");
     formationSection.classList.add("formation");
-    formationSection.innerHTML = ` 
+    formationSection.innerHTML = `
             <div class="GK">${fillpositionrand("GK", jsonArrayFttd)}<div class="dot">GK</div></div>
 
-            <div class="CBL">${fillpositionrand("CB", jsonArrayFttd)}<div class="dot">CBL</div></div>
+            <div class="CBL">${fillpositionrand("CB", jsonArrayFttd)}<div class="dot">CB</div></div>
 
-            <div class="CBR">${fillpositionrand("CB", jsonArrayFttd)}<div class="dot">CBR</div></div>
+            <div class="CBR">${fillpositionrand("CB", jsonArrayFttd)}<div class="dot">CB</div></div>
 
             <div class="LB">${fillpositionrand("LB", jsonArrayFttd)} <div class="dot">LB</div></div>
 
             <div class="RB"> ${fillpositionrand("RB", jsonArrayFttd)}<div class="dot">RB</div></div>
 
-            <div class="MDF">${fillpositionrand("CM", jsonArrayFttd)}<div class="dot">MDF</div></div>
+            <div class="MDF">${fillpositionrand("CM", jsonArrayFttd)}<div class="dot">CM</div></div>
 
-            <div class="ML">${fillpositionrand("CM", jsonArrayFttd)}<div class="dot">ML</div></div>
+            <div class="ML">${fillpositionrand("CM", jsonArrayFttd)}<div class="dot">CM</div></div>
 
-            <div class="MR">${fillpositionrand("CM", jsonArrayFttd)}<div class="dot">MR</div></div>
+            <div class="MR">${fillpositionrand("CM", jsonArrayFttd)}<div class="dot">CM</div></div>
 
-            <div class="STM">${fillpositionrand("ST", jsonArrayFttd)}<div class="dot">STM</div></div>
+            <div class="STM">${fillpositionrand("ST", jsonArrayFttd)}<div class="dot">ST</div></div>
 
-            <div class="FWR">${fillpositionrand("RW", jsonArrayFttd)}<div class="dot">FWR</div></div>
+            <div class="FWR">${fillpositionrand("RW", jsonArrayFttd)}<div class="dot">RW</div></div>
 
-            <div class="FWL">${fillpositionrand("LW", jsonArrayFttd)}<div class="dot">FWL</div></div> 
+            <div class="FWL">${fillpositionrand("LW", jsonArrayFttd)}<div class="dot">LW</div></div>
+            
+
             `
     mainHtml.appendChild(formationSection);
 }
@@ -86,11 +87,10 @@ function fillpositionrand(posStr, jsonArrayFpr)
             if (player.position === posStr)
             {
                 player_found = player;
-                console.log(player_found);
+                
                 break;
             }
         }
-        console.log("from fillpositionrand() player_found =" + player_found);
         let playersBadge = 
         `
                 <div class="playerCard">
@@ -110,8 +110,13 @@ function fillpositionrand(posStr, jsonArrayFpr)
                 </div>
             </div>
             `
-        return (playersBadge)
+        return (playersBadge);
         
+}
+
+function checkdot ()
+{
+    console.log(positionDot);
 }
 async function Apicons() 
 {
@@ -119,15 +124,122 @@ async function Apicons()
     let converted = await fetched.json();
     return (converted);
 }
+function renderPlayerModal (arrplayersRpm, pos)
+{
+    let modal = document.querySelector(".modal");
+    for (let player of arrplayersRpm)
+    {
+        let playersBadge = document.createElement("div");
+        playersBadge.classList.add(pos)
+        playersBadge.innerHTML =
+        `
+            <div class="playerCard">
+            <div class="rating"> <span class="ratingnum">${player.rating}</span> <span class="position">${player.position}</span></div>
+            <div class="ppicture"><img src="${player.photo}" alt=""></div>
+            <div class="pname">${player.name}</div>
+            <div class="PAC">PAC ${player.pace}</span></div>
+            <div class="SHO">SHO ${player.shooting}</div>
+            <div class="PAS">PAS ${player.passing}</div>
+            <div class="DRI">DRI ${player.dribbling}</div>
+            <div class="DEF">DEF ${player.defending}</div>
+            <div class="PHY">PHY ${player.physical}</div>
+            <div class="logos">
+            <div class="flag"><img  src="${player.flag}" alt=""></div>
+            <div class="logo"><img class="logo" src="${player.logo}" alt=""></div>
+            <div class="club"><img  src="${player.logo}" alt=""></div>
+            </div>
+            </div>
+            <div class="dot">${player.position}</div>
+        `
 
+        // playerinmodal = querySelector(".modal ")
+        modal.appendChild(playersBadge);
+
+    }
+}
+    // let dotinmodal = document.querySelectorAll(".modal .dot");
+    // Array.from(dotinmodal).forEach(dot => 
+    // {
+    //     dot.addEventListener("click", () =>
+    //     {
+    //         // element.parentNode.removeChild(element);
+    //         let formationSection = document.querySelector(".formation");
+    //         let captured = document.querySelector(`.formation .${pos}`)
+    //         // console.log("captured below")
+    //         captured.parentNode.removeChild(captured);
+    //         // console.log(captured);
+          
+    //         captured.remove();
+    //         let modal = document.querySelector(".modal")
+    //         let cardInModal = document.querySelector(`.modal .${pos}`);
+    //         console.log("cardinModal" +cardInModal);
+    //         // console.log("cardinModal" + cardInModal);
+    //         formationSection.appendChild(cardInModal);
+    //         formationSection.style.display = "grid";
+    //         modal.style.display = "none";
+        //     let dot = document.querySelectorAll(".dot");
+        //     Array.from(dot).forEach(doty => 
+        //         {
+        //             doty.addEventListener("click", ()=>
+        //             {
+        //                 let formationSection = document.querySelector(".formation")
+        //                 let modal = document.querySelector(".modal");
+        //                 formationSection.style.display = "none";
+        //                 modal.style.display = "flex";
+        //                 positionDot = doty.innerText;
+        //                 // console.log("doty parent" + doty.parentElement.className);
+        //                 renderPlayerModal(showAllPos(positionDot, arrplayersRpm), doty.parentElement.className);
+        //                 // showAllPos(positionDot, jsonArr);
+        //                 // console.log(positionDot);
+        //                 // console.log(formationSection);
+        //             })
+        //         }
+        //         )
+        // }
+    // )
+//     })
+// })}
+
+function showAllPos (posStr, jsonArrSap)
+{
+    let arrplayers = [];
+    for (let player of jsonArrSap)
+    {
+        if (player.position === posStr)
+        {
+            arrplayers.push(player);
+        }
+    }
+    return (arrplayers);
+}
 async function main()
 {
-    
     // let jsonArray = await Apicons();
-    let jsonArr =  await Apicons(); 
-    console.log(jsonArr);
+    let jsonArr =  await Apicons();
+    let positionDot;
     fourThreeThreeDefault(jsonArr);
+    let dot = document.querySelectorAll(".dot");
+    Array.from(dot).forEach(doty => 
+        {
+            doty.addEventListener("click", ()=>
+            {
+                let formationSection = document.querySelector(".formation")
+                let modal = document.querySelector(".modal");
+                formationSection.style.display = "none";
+                modal.style.display = "flex";
+                positionDot = doty.innerText;
+                // console.log("doty parent" + doty.parentElement.className);
+                renderPlayerModal(showAllPos(positionDot, jsonArr), doty.parentElement.className);
+                // showAllPos(positionDot, jsonArr);
+                // console.log(positionDot);
+                // console.log(formationSection);
+            })
+        }
+        )
+    
+    
     // fillpositionrand("CB", jsonArray);
+
 }
 main();
 
